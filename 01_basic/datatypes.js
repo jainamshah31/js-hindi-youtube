2cd8d9cd3311a329e528ae4a6889a86f8672af7a
jainamshah31 <jainamshah383@gmail.com>
2024-07-02 06:34:05 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // trat all js code as newer version
+
+//alert(2+3) // we are using nodejs not browser
+
+console.log(3+2);
+
+console.log("jainam");
+
+const name = "jainamshah" // String
+let age = 25 // Numbers
+let isLoggedIn = false //boolean
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+// null => standvalue (object)
+// undefined => undefined 
+// let isLoggedIn => boolean
+// let string => name
+// symbol => unique

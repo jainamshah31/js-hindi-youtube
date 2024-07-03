c4d9e245902c66e5ac57de06ce634b88a9a7f002
jainamshah31 <jainamshah383@gmail.com>
2024-07-03 09:43:02 +0000

    datatypes,stack

---
+++ 01_basic/datatypes2.js
@@ -0,0 +1,12 @@
+const Id= '123'
+const anotherId = Symbol('1234')
+
+console.log(Id===anotherId);
+
+// link of study 
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+               //Memory 
+//(1)Stack Memory
+//(2)Heap Memory

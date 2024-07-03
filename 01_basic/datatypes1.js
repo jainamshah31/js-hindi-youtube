5a12ed98c60122d8a5eb6f606d72f8de3562cb05
jainamshah31 <jainamshah383@gmail.com>
2024-07-03 04:10:13 +0000

    datatypes1 implemation

---
+++ 01_basic/datatypes1.js
@@ -0,0 +1,25 @@
+let score="jainam"
+
+//console.log(typeof score);
+//let valueInNumber = Number(score)
+//console.log(valueInNumber);
+//33 =>number 
+// "33abs" => NaN
+// null => object
+// typeof Score => String
+// valueInNumber => NaN
+// true => 1 & false => 0;
+
+let isLoggedIn;
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log( booleanisLoggedIn);
+
+let someNumber = 25
+let StringNumber = String(someNumber);
+
+console.log(StringNumber);
+
+//1 => true
+//0 => false
+// "" =>false
+//"jainam"=>true

5ad2b82fdf32d3629429c7917f1d43fea082bf74
jainamshah31 <jainamshah383@gmail.com>
2024-07-05 05:17:41 +0000

    datatypes2

--- 01_basic/datatypes2.js
+++ 01_basic/datatypes2.js
@@ -1,12 +1,28 @@
 const Id= '123'
 const anotherId = Symbol('1234')
 
-console.log(Id===anotherId);
+//console.log(Id===anotherId);
 
 // link of study 
 //https://262.ecma-international.org/5.1/#sec-11.4.3
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
                //Memory 
-//(1)Stack Memory
-//(2)Heap Memory
+//(1)Stack Memory (primitive type use)
+//(2)Heap Memory  (non- primitive data type use)
+
+let myYoutubename = "jainamshah"
+
+let anotherName = myYoutubename 
+anotherName = "chaiaurcode"
+console.log(anotherName)
+
+let userOne = {
+    email:"jainamshah@.com"
+    Upi:"usre@hdfc"
+}
+let userTwo = userOne
+
+userTwo.email="jainamshah@.com"
+console.log(userOne.email);
+console.log(userTwo.email);

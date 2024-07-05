7681618ba57db33b52a85f249cacf71d36a2c90b
jainamshah31 <jainamshah383@gmail.com>
2024-07-05 05:12:43 +0000

    Strings name reverse

---
+++ 01_basic/Strings.js
@@ -0,0 +1,21 @@
+const name = "jainam"
+const repoCount = 50
+
+//console.log(name + repoCount + "value");
+
+const gameName = new String('jainamjk')
+
+//console.log(gameName[1]);
+//console.log(gameName.__proto__);
+
+const anotherString = gameName.slice(-2,3)
+//console.log(gameName.slice);
+
+const newString = gameName.substring(0,4)
+
+console.log(newString);
+
+//slice => Reverse
+//Replece => change- add
+//subString => reverse full name
+
